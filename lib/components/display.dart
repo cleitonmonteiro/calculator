91eee3cb2debaf25b980ca44f1c74cbd325a1a60
Cleiton Monteiro <cleitonmonteiro.dev@gmail.com>
2020-02-07 11:39:36 -0300

    Builded base display widget

--- lib/components/display.dart
+++ lib/components/display.dart
@@ -1,14 +1,39 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 
 class Display extends StatelessWidget {
   final String text;
 
   Display(this.text);
-
+  
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text(text),    
+    return Expanded (
+      flex: 1,
+      child: Container(
+        color: Color.fromRGBO(48, 48, 48, 1),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: AutoSizeText(
+                text,
+                minFontSize: 20,
+                maxFontSize: 80,
+                maxLines: 1,
+                textAlign: TextAlign.end,
+                style: TextStyle(
+                  fontWeight: FontWeight.w100,
+                  decoration: TextDecoration.none,
+                  color: Colors.white
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
